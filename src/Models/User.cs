f3ba24d4bad631293ec8837dec774aa45b9aadf6
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-14 15:02:56 +0800

    feature: UserLoginInterface

---
+++ src/Models/User.cs
@@ -0,0 +1,10 @@
+namespace MY_APP.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] Salt { get; set; }
+    }
+}

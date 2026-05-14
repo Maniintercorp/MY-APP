547ef8f3c42e235590f790491693e6482a95e07e
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-14 17:18:34 +0800

    feature: LoginFormWithBackend

---
+++ src/features/LoginFormWithBackend/types/index.ts
@@ -0,0 +1,14 @@
+export interface LoginFormType {
+  email: string;
+  password: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+  userId: string;
+}

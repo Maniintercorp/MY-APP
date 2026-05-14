6f68f0c4ee8380076508c5a3d79822e87bf49125
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-14 16:47:20 +0800

    feature: LoginForm

--- src/DTOs/LoginRequestDto.cs
+++ src/DTOs/LoginRequestDto.cs
@@ -5,7 +5,8 @@ namespace MY_APP.DTOs
     public class LoginRequestDto
     {
         [Required]
-        public string Username { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
 
         [Required]
         public string Password { get; set; }

6bca44dae8d7add88f599bf982e1725da0c0be71
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-14 16:47:29 +0800

    feature: LoginForm

--- Database/StoredProcedures/usp_UpdateUser.sql
+++ Database/StoredProcedures/usp_UpdateUser.sql
@@ -1,14 +1,14 @@
 CREATE PROCEDURE usp_UpdateUser
-    @UserId INT,
-    @Email NVARCHAR(255),
-    @PasswordHash NVARCHAR(255)
+	@UserId INT,
+	@Email NVARCHAR(256),
+	@PasswordHash VARBINARY(256)
 AS
 BEGIN
-    SET NOCOUNT ON;
-    
-    UPDATE Users
-    SET Email = @Email, 
-        PasswordHash = @PasswordHash, 
-        UpdatedAt = GETUTCDATE()
-    WHERE UserId = @UserId AND IsDeleted = 0;
+	SET NOCOUNT ON;
+
+	UPDATE Users
+	SET Email = @Email, 
+	    PasswordHash = @PasswordHash, 
+	    UpdatedAt = GETUTCDATE()
+	WHERE UserId = @UserId;
 END

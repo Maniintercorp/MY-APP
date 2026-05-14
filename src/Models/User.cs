2af941c7f1a1536df74fbdc22f9c48da5f29e486
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-14 16:47:10 +0800

    feature: LoginForm

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,12 +1,9 @@
-namespace MyApp.Models
+namespace MY_APP.Models
 {
     public class User
     {
         public int UserId { get; set; }
-        public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public bool IsDeleted { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
     }
 }

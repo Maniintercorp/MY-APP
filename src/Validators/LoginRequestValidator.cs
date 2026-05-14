7528fc4501aa878ac442c9468807cbff8894f560
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-14 17:19:00 +0800

    feature: LoginFormWithBackend

---
+++ src/Validators/LoginRequestValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+using MyApp.DTOs;
+
+namespace MyApp.Validators
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequestDto>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}

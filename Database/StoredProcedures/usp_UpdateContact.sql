e1b45fd44862a913e7d8614b1e04257115413f8d
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-15 20:43:44 +0800

    feature: ContactFormFeature

---
+++ Database/StoredProcedures/usp_UpdateContact.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE usp_UpdateContact
+    @Id INT,
+    @Name NVARCHAR(100),
+    @Email NVARCHAR(100),
+    @Message NVARCHAR(500)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    UPDATE Contacts
+    SET Name = @Name, Email = @Email, Message = @Message, UpdatedAt = GETDATE()
+    WHERE Id = @Id AND IsDeleted = 0;
+END

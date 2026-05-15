82e847ba1ce421e0df87eaddab361614719e48a8
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-15 20:43:32 +0800

    feature: ContactFormFeature

---
+++ src/Models/Contact.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}

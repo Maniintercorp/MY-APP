a0f81d5d504bd8fa2bff93a7d119e0385d2241f8
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-15 21:13:16 +0800

    feature: ContactFormFeature (backend)

---
+++ Database/StoredProcedures/usp_DeleteContact.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE usp_DeleteContact
+    @Id INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    UPDATE Contacts
+    SET IsDeleted = 1,
+        UpdatedAt = GETDATE()
+    WHERE Id = @Id;
+END
+GO

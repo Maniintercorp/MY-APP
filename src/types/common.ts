67192896939dd13af6d63fe20f3af6d750e8e8c4
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-15 21:55:05 +0800

    feature: LoginAndSignupSplitPanel

---
+++ src/types/common.ts
@@ -0,0 +1,14 @@
+export interface ApiResponse<T> {
+  data: T;
+  message: string;
+}
+
+export interface PaginatedResponse<T> extends ApiResponse<T[]> {
+  total: number;
+  page: number;
+  pageSize: number;
+}
+
+export interface ApiError {
+  message: string;
+}

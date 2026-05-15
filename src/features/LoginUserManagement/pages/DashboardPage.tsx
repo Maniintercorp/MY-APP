cfd8d398fbd26dc83c3a77cf029df85cfc43a779
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-15 16:52:58 +0800

    feature: LoginUserManagement

---
+++ src/features/LoginUserManagement/pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <p>Welcome to your dashboard!</p>
+    </div>
+  );
+};
+
+export default DashboardPage;

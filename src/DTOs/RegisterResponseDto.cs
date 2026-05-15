a329d2272b36f0802e3ba2d6024c67607b5a57e1
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-15 16:53:33 +0800

    feature: LoginUserManagement

---
+++ src/DTOs/RegisterResponseDto.cs
@@ -0,0 +1,9 @@
+namespace DTOs
+{
+    public class RegisterResponseDto
+    {
+        public int UserId { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}

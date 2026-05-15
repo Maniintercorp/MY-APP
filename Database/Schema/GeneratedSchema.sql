2c0a94f8df8dd034c4493d9e89c77682c9e76661
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-15 16:38:21 +0800

    feature: TableDDLGeneration

--- Database/Schema/GeneratedSchema.sql
+++ Database/Schema/GeneratedSchema.sql
@@ -1,18 +1,14 @@
--- SQL script to create the Users table based on analysed stored procedures
-
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Users')
-BEGIN
-    CREATE TABLE Users
-    (
-        UserId UNIQUEIDENTIFIER PRIMARY KEY,
-        Email NVARCHAR(255) NOT NULL,
-        PasswordHash VARBINARY(MAX) NOT NULL,
-        CreatedAt DATETIME2 NOT NULL,
-        UpdatedAt DATETIME2 NOT NULL,
-        IsDeleted BIT DEFAULT 0,
-        INDEX IX_Users_Email (Email)
-    );
-END
+-- CREATE TABLE for Users table
+CREATE TABLE Users
+(
+    UserId UNIQUEIDENTIFIER PRIMARY KEY,
+    Email NVARCHAR(255) NOT NULL,
+    PasswordHash VARBINARY(MAX) NOT NULL,
+    CreatedAt DATETIME2 NOT NULL,
+    UpdatedAt DATETIME2 NOT NULL,
+    IsDeleted BIT DEFAULT 0,
+    INDEX IX_Users_Email (Email)
+);
 GO
 
 -- Analysed stored procedures: usp_CreateUser, usp_ReadUser, usp_UpdateUser, usp_DeleteUser

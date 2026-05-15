e4f193e638481faf0f08616ea53cf5010290665f
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-15 21:55:23 +0800

    feature: LoginAndSignupSplitPanel

---
+++ src/features/LoginAndSignupSplitPanel/pages/LoginAndSignupSplitPanelPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { LoginAndSignupSplitPanel } from '../components';
+
+export const LoginAndSignupSplitPanelPage: React.FC = () => (
+  <div className="container mx-auto">
+    <LoginAndSignupSplitPanel />
+  </div>
+);

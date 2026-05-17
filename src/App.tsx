f6b5ec6b59d7909331e23af5edace01b3d193510
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-18 02:06:30 +0800

    feature: MultilingualSupport

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Layout } from '@/components/Layout';
-import { LoginPage } from '@/features/LoginAndRegistrationPages/pages/LoginPage';
-import { RegistrationPage } from '@/features/LoginAndRegistrationPages/pages/RegistrationPage';
-import { FeaturePage } from '@/features/LoginAndRegistrationPages/pages/FeaturePage';
+import {{ Routes, Route, Navigate }} from 'react-router-dom';
+import { LanguageSettingsPage } from '@/features/MultilingualSupport/pages/LanguageSettingsPage.tsx';
 
 export const App = () => (
   <Routes>
-    {/* Auth routes -- full-screen, NO sidebar/navbar */}
-    <Route path="/login"    element={<LoginPage />} />
-    <Route path="/register" element={<RegistrationPage />} />
-    <Route path="/"         element={<Navigate to="/login" replace />} />
-    {/* App routes -- always inside Layout */}
-    <Route element={<Layout />}>
-      <Route path="/feature"   element={<FeaturePage />} />
-    </Route>
+    <Route path="/" element={<Navigate to="/languagesettings" replace />} />
+        <Route path="/languagesettings" element={<LanguageSettingsPage />} />
   </Routes>
 );

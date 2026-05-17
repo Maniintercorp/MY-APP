57c87a7bb7523f0a62aea2fddc7cbb30b4ee99a8
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-18 01:41:15 +0800

    feature: LoginPageRedesign

---
+++ src/features/LoginPageRedesign/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { AuthLayout, LoginCard } from '@/features/LoginPageRedesign/components';
+
+export const LoginPage = () => {
+  return (
+    <AuthLayout>
+      <LoginCard />
+    </AuthLayout>
+  );
+};
+
+export default LoginPage;

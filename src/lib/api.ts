ac3a819944c7fff921feb3eed8e833eaafe665a1
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-18 01:41:18 +0800

    feature: LoginPageRedesign

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,2 +1,10 @@
-// auto-stub
-export {};
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? '',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;

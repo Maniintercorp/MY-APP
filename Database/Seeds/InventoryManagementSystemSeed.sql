b5208f17aecd348c5321f663d486c53ac652268a
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-19 18:22:32 +0800

    feature: InventoryManagementSystem

---
+++ Database/Seeds/InventoryManagementSystemSeed.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbo.Categories (Name, Description)
+VALUES
+    ('General', 'Default inventory category'),
+    ('Raw Materials', 'Materials used to manufacture or assemble products'),
+    ('Finished Goods', 'Completed products available for sale');

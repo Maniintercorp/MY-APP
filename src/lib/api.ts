d9487b522b0d72a91ab6a237f90fc5d1e10a0218
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-21 22:47:17 +0800

    feature: LoginAndRegistrationPages

---
+++ src/lib/api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
+});
+api.interceptors.response.use(
+  (r) => r,
+  (err) => {
+    if (err.response?.status === 401) window.location.href = '/login';
+    return Promise.reject(err);
+  }
+);
+export default api;

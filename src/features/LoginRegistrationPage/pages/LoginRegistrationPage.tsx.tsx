1d733d80fbfd0727e688ccfb781901799c5311aa
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-21 18:55:58 +0800

    feature: LoginRegistrationPage

---
+++ src/features/LoginRegistrationPage/pages/LoginRegistrationPage.tsx.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loginregistrationpage.tsx = () => (
+  <div className="p-8 text-gray-400">Loginregistrationpage.tsx</div>
+);
+
+export default Loginregistrationpage.tsx;
+export { Loginregistrationpage.tsx };
+export { Loginregistrationpage };

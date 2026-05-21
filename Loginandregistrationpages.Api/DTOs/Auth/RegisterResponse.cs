ee961bc94ee3d3df3f888e1a05e4db639140453e
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-21 22:48:36 +0800

    feature: LoginAndRegistrationPages

---
+++ Loginandregistrationpages.Api/DTOs/Auth/RegisterResponse.cs
@@ -0,0 +1,9 @@
+namespace Loginandregistrationpages.Api.DTOs.Auth
+{
+    public class RegisterResponse
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}

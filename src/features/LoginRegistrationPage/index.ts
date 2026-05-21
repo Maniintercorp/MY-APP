72c4ed12d9f4a14c72c0ee5982f6bb143bf71469
Maniintercorp <manikandan.arumugam@intercorpsolutions.in>
2026-05-21 18:55:37 +0800

    feature: LoginRegistrationPage

---
+++ src/features/LoginRegistrationPage/index.ts
@@ -0,0 +1,5 @@
+export * from '@/features/LoginRegistrationPage/types';
+export * from '@/features/LoginRegistrationPage/services';
+export * from '@/features/LoginRegistrationPage/hooks';
+export * from '@/features/LoginRegistrationPage/components';
+export * from '@/features/LoginRegistrationPage/pages';
